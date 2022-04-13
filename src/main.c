e805fbf2ed85f15f482020ac548e4e6778f76601
a-cs <acarneirosousa@gmail.com>
2022-04-13 18:40:11 -0300

    has_duplicates

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: acarneir <acarneir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 22:12:24 by acarneir          #+#    #+#             */
-/*   Updated: 2022/04/12 01:52:23 by acarneir         ###   ########.fr       */
+/*   Updated: 2022/04/13 18:37:21 by acarneir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@ void	print_error(void)
 {
 	ft_putstr_fd("Error\n", 2);
 	exit(1);
+}
+
+void	clear_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
 }
 
 char	*get_args_str(int argc, char **argv)
@@ -41,17 +54,13 @@ char	*get_args_str(int argc, char **argv)
 
 void	is_valid_args_str(char *str)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		// if (str[i] != ' ' && !(str[i] == '+' && ft_isdigit(str[i + 1]))
-		// 	&& !(str[i] == '-' && ft_isdigit(str[i + 1]))
-		// 	&& !ft_isdigit(str[i]))
 		if (str[i] != ' ' && !((str[i] == '+' || str[i] == '-')
-				&& ft_isdigit(str[i + 1]))
-			&& !ft_isdigit(str[i]))
+				&& ft_isdigit(str[i + 1])) && !ft_isdigit(str[i]))
 		{
 			ft_printf("str = %c%c\n", str[i], str[i + 1]);
 			free(str);
@@ -70,64 +79,76 @@ int	is_valid_int(char **args)
 	while (args[i])
 	{
 		if (ft_strlen(args[i]) > 11)
-			return (1);
+			return (0);
 		else if (ft_strlen(args[i]) == 11
 			&& ft_memcmp("-2147483648", args[i], 11) < 0)
-			return (1);
+			return (0);
 		else if (ft_strlen(args[i]) == 11 && args[i][0] == '+'
 			&& ft_memcmp("+2147483647", args[i], 11) < 0)
-			return (1);
+			return (0);
 		else if (ft_strlen(args[i]) == 10
 			&& ft_memcmp("2147483647", args[i], 10) < 0)
-			return (1);
+			return (0);
 		else if (ft_atoi(args[i]) == 0
 			&& (ft_memcmp("+0", args[i], ft_strlen(args[i])) != 0
 				&& ft_memcmp("-0", args[i], ft_strlen(args[i])) != 0
 				&& ft_memcmp("0", args[i], ft_strlen(args[i])) != 0))
-			return (1);
+			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
 }
 
-void	clear_args(char **args)
+void	has_duplicates(char **args)
 {
 	int	i;
+	int	j;
+	int	value;
 
 	i = 0;
 	while (args[i])
 	{
-		// ft_printf("i -> %d args -> %s\n", i, args[i]);
-		// ft_printf("str = %s -> atoi = %d\n", args[i], ft_atoi(args[i]));
-		free(args[i]);
+		value = ft_atoi(args[i]);
+		j = i + 1;
+		while (args[j])
+		{
+			if (value == ft_atoi(args[j]))
+			{
+				ft_printf("i = %d, arg[%d] = %d, v = %d\n", i, j, ft_atoi(args[j]), value);
+				clear_args(args);
+				ft_printf("flag_d\n");
+				print_error();
+			}
+			j++;
+		}
 		i++;
 	}
-	free(args);
 }
 
-// pode receber "1 2 3" ou sem aspas
 int	main(int argc, char **argv)
 {
 	char	**args;
 	char	*args_str;
-	int		flag2;
-	// ft_printf("+-42 %d\n" ,ft_atoi("+-42"));
-	// ft_strncmp(str, "0", ft_strlen(str))
 
 	if (argc >= 2)
 	{
 		args_str = get_args_str(argc, argv);
+		ft_printf("args_str -> %s\n", args_str);
 		is_valid_args_str(args_str);
 		args = ft_split(args_str, ' ');
-		flag2 = is_valid_int(args);
-		clear_args(args);
-		// valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --tool=memcheck -q ./push_swap "1 2" "3 4 5"  "+-1"
-		ft_printf("args_str -> %s\n", args_str);
 		free(args_str);
-		ft_printf("flag2 -> %d\n", flag2);
-		if (flag2)
+		if (is_valid_int(args))
+		{
+			has_duplicates(args);
+			ft_printf("OK");
+			clear_args(args);
+		}
+		else
+		{
+			clear_args(args);
+			ft_printf("flag_int\n");
 			print_error();
+		}
 	}
-
 	return (0);
 }
